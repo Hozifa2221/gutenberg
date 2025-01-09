4517ec39ad65ae2de6141820483b71e828faa974
George Mamadashvili <georgemamadashvili@gmail.com>
2025-01-09 14:37:24 +0400

    Restore default rendering mode for Pages

--- lib/load.php
+++ lib/load.php
@@ -97,6 +97,7 @@ require __DIR__ . '/compat/wordpress-6.7/post-formats.php';
 require __DIR__ . '/compat/wordpress-6.8/preload.php';
 require __DIR__ . '/compat/wordpress-6.8/blocks.php';
 require __DIR__ . '/compat/wordpress-6.8/functions.php';
+require __DIR__ . '/compat/wordpress-6.8/post.php';
 require __DIR__ . '/compat/wordpress-6.8/site-editor.php';
 require __DIR__ . '/compat/wordpress-6.8/class-gutenberg-rest-user-controller.php';
 
